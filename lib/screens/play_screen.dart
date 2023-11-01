cd9648fda265c2dcacba77b59f288c46f032c8d5
DungxLas <85223326+DungxLas@users.noreply.github.com>
2023-11-01 07:52:21 +0700

    31/10/2023

--- lib/screens/play_screen.dart
+++ lib/screens/play_screen.dart
@@ -1,9 +1,9 @@
 import 'package:card_memory_game/data/model/image_q.dart';
 import 'package:card_memory_game/widgets/countdown_timer.dart';
+import 'package:card_memory_game/widgets/gridItem.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../widgets/item.dart';
 import '../providers/card_provider.dart';
 
 class PlayScreen extends ConsumerStatefulWidget {
@@ -14,28 +14,27 @@ class PlayScreen extends ConsumerStatefulWidget {
 }
 
 class _PlayScreenState extends ConsumerState<PlayScreen> {
-  List<Widget> gridCard = <Widget>[];
-  late List<ImageQ> cardData;
+  //List<Widget> gridCard = <Widget>[];
 
-  void hideItem(String id) {
-    int index1 = cardData.indexWhere((element) => element.id == id);
-    int index2 = cardData.indexWhere((element) => element.id == id, index1);
+  // void hideItem(String id) {
+  //   int index1 = cardData.indexWhere((element) => element.id == id);
+  //   int index2 = cardData.indexWhere((element) => element.id == id, index1);
 
-    ImageQ xxx = const ImageQ(id: 'xxx', front: 'lib/assets/image/OIG.jpg');
+  //   ImageQ xxx = const ImageQ(id: 'xxx', front: 'lib/assets/image/OIG.jpg');
 
-    setState(() {
-      // gridCard[index1] = xxx as Widget;
-      // gridCard[index2] = xxx as Widget;
-      // gridCard.removeAt(index1);
-      // gridCard.removeAt(index2);
-      gridCard = [];
-    });
-  }
+  //   setState(() {
+  //     // gridCard[index1] = xxx as Widget;
+  //     // gridCard[index2] = xxx as Widget;
+  //     // gridCard.removeAt(index1);
+  //     // gridCard.removeAt(index2);
+  //     gridCard = [];
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
-    cardData = ref.watch(listCard);
-    
+    List<ImageQ> cardData = ref.watch(listCard);
+
     return Scaffold(
       body: Container(
         margin: const EdgeInsets.only(top: 30),
@@ -44,7 +43,7 @@ class _PlayScreenState extends ConsumerState<PlayScreen> {
             const CountdownTimer(
               start: 30,
             ),
-            ,
+            GridItem(data: cardData),
           ],
         ),
       ),
--- lib/widgets/gridItem.dart
+++ lib/widgets/gridItem.dart
@@ -1,11 +1,12 @@
+import 'package:card_memory_game/data/collection.dart';
+import 'package:card_memory_game/widgets/item1.dart';
+import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
 
 import '../data/model/image_q.dart';
-import 'item.dart';
 
 class GridItem extends StatefulWidget {
   const GridItem({super.key, required this.data});
-
   final List<ImageQ> data;
 
   @override
@@ -13,33 +14,96 @@ class GridItem extends StatefulWidget {
 }
 
 class _GridItemState extends State<GridItem> {
+  late List<GlobalKey<FlipCardState>> cardKeys;
+  List<int> flippedIndices = [];
+  late List<bool> listFlipOnTouch;
+  List<int> itemMatch = [];
+
+  @override
+  void initState() {
+    super.initState();
+    cardKeys = List.generate(
+        widget.data.length, (index) => GlobalKey<FlipCardState>());
+    listFlipOnTouch = List<bool>.filled(widget.data.length, true);
+  }
+
+  void process(int index) {
+    if (flippedIndices.length < 2) {
+      flippedIndices.add(index);
+    }
+    if (flippedIndices.length == 2) {
+      setState(() {
+        listFlipOnTouch[flippedIndices[0]] = true;
+        listFlipOnTouch[flippedIndices[1]] = true;
+        for (var i = 0; i < widget.data.length; i++) {
+          if (i != flippedIndices[0] || i != flippedIndices[1]) {
+            listFlipOnTouch[i] = false;
+          }
+        }
+      });
+    }
+  }
+
+  void check() {
+    if (flippedIndices.length == 2) {
+      String item1 = widget.data[flippedIndices[0]].id;
+      String item2 = widget.data[flippedIndices[1]].id;
+      setState(() {
+        if (item1 == item2) {
+          itemMatch.add(flippedIndices[0]);
+          itemMatch.add(flippedIndices[1]);
+          for (var i = 0; i < listFlipOnTouch.length; i++) {
+            if (itemMatch.contains(i)) {
+              listFlipOnTouch[i] = false;
+            } else {
+              listFlipOnTouch[i] = true;
+            }
+          }
+          flippedIndices.clear();
+        } else {
+          Future.delayed(const Duration(milliseconds: 300), () {
+            cardKeys[flippedIndices[0]].currentState!.toggleCard();
+            cardKeys[flippedIndices[1]].currentState!.toggleCard();
+          });
+        }
+      });
+    }
+  }
+
+  void reMatch() {
+    setState(() {
+      for (var i = 0; i < listFlipOnTouch.length; i++) {
+        if (itemMatch.contains(i)) {
+          listFlipOnTouch[i] = false;
+        } else {
+          listFlipOnTouch[i] = true;
+        }
+      }
+      flippedIndices.clear();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    List<Widget> gridCard = widget.data.map((item) {
-      return Item(
-        image: item,
-        onHideItem: () {
-          //hideItem(item.id);
-          // setState(() {
-          //   gridCard = [];
-          // });
-        },
-      );
-    }).toList();
     return Expanded(
       child: GridView.builder(
-        itemCount: gridCard.length + 1,
+        itemCount: widget.data.length,
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 3,
           crossAxisSpacing: 4,
           mainAxisSpacing: 8,
         ),
         itemBuilder: (BuildContext context, int index) {
-          if (index == 0) {
-            return const SizedBox(height: 50, width: 50);
-          } else {
-            return gridCard[index - 1];
-          }
+          return Item1(
+            image: widget.data[index],
+            cardKey: cardKeys[index],
+            flipOnTouch: listFlipOnTouch[index],
+            onFlip: () {
+              process(index);
+            },
+            onFlipB2FDone: check,
+            onFlipF2BDone: reMatch,
+          );
         },
       ),
     );
